098bc42f949e79ff6bf1ad00cc70c4cd4f0f9003
Felix Jordan <felixjordan99@gmx.de>
2020-03-07 17:10:48 +0100

    Update push.sh

--- bin/push.sh
+++ bin/push.sh
@@ -3,6 +3,6 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-    docker push softinstigate/maven-aws:latest;
-    docker push softinstigate/maven-aws:"$RELEASE";
+    docker push buddyactivities/maven-aws:latest;
+    docker push buddyactivities/maven-aws:"$RELEASE";
 fi
